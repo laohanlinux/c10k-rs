fa54f0550ca6576aa6af1d614538eb53fb040d2b
Rg <daimaldd@gmail.com>
2019-04-15 22:34:55 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use tokio::prelude::*;
+use tokio::net::TcpListener;
+use futures::{future, Future};
+
+fn main() {
+    let addr = "0.0.0.0:3941".parse().unwrap();
+    let lis = TcpListener::bind(&addr).unwrap();
+    println!("listen: {}", addr.to_string());
+
+    let incomming = lis.incoming();
+
+    let server = incomming.map_err(|e| eprintln!("accept failed = {:?}", e))
+        .for_each(|socket|{
+            let (rd, wt) = socket.split();
+            let bytes_copied = tokio::io::copy(rd, wt);
+            let callback = bytes_copied.map(|(_,_, _) | {
+            }).map_err(|e| eprintln!("IO error {:?}", e));
+
+            tokio::spawn(callback);
+            future::ok(())
+        });
+
+
+    tokio::run(server);
+}
