4d0dd5a27c1c02506cef8301f3b645a819cdac07
Rg <daimaldd@gmail.com>
2019-04-25 12:27:59 +0800

    remove

--- examples/runtime-benchmark.rs
+++ examples/runtime-benchmark.rs
@@ -1,21 +1,15 @@
-#![feature(async_await, await_macro, futures_api)]
+#![feature(async_await, await_macro)]
 
 use c10k_rs::runtime_io::runtime_io;
-use c10k_rs::runtime_io::timer;
-use c10k_rs::bucket::Buckets;
-use futures::future::ok;
-use std::thread;
-use rand::Rng;
-use runtime::net::tcp::{TcpListener, Incoming};
-use std::net::SocketAddr;
-use futures::prelude::*;
 
 #[runtime::main]
 async fn main() {
-
     runtime::spawn(async move {
         println!("hello Word");
     });
 
-    await!(runtime_io());
+    let result  = await!(runtime_io());
+    if result.is_err() {
+        println!("{:?}", result);
+    }
 }
